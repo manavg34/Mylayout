9c056937a2b9427e5af155ec80e5f6cf157f65be
EC2 Default User <ec2-user@ip-172-31-19-226.us-east-2.compute.internal>
2018-09-20 09:04:44 +0000

    edit profile func added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user       #points to user_url i.e. show method
       # Handle a successful save.
@@ -20,7 +21,18 @@ class UsersController < ApplicationController
  def edit
     @user = User.find(params[:id])
   end
-
+  
+ def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "Profile updated"
+      redirect_to @user
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
