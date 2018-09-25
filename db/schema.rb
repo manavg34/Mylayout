2aaf2105767358089cd87140f79a0fb33bdda5c9
EC2 Default User <ec2-user@ip-172-31-19-226.us-east-2.compute.internal>
2018-09-25 18:03:26 +0000

    making admin users

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920164340) do
-
-  create_table "sampledevises", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_sampledevises_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_sampledevises_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 20180924164751) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -30,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180920164340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
